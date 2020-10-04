afabfe8c22d45bcac210ebdb9ae30d1f3d541249
KPH3802 <kph3802@gmail.com>
2020-10-04 14:20:48 -0500

    Adding first scraper.py and webscraping.py

---
+++ scraper.py
@@ -0,0 +1,40 @@
+def scrape_site(soup):
+    links = []
+    #Get all the links to each contact
+    for link in soup.find_all("a", class_="item", href=True):
+        links.append(link.get('href'))
+    #Get Next button
+    for link in soup.find_all("a", class_="btn btn--primary pull-right", href=True):
+        button = link.get('href')
+
+    dict_list =[]
+    for i in links:
+        if '/houston/potmsearch/detail/submission' in i:
+            d = {}
+            link = "https://www.bizjournals.com/" + i
+            newI = i.split("?")
+            slicefront = (newI[0])
+            name = slicefront[46:]
+            final_name = name.replace('_', " ")
+            d["Name"] = final_name
+            d["Link"] = link
+            dict_list.append(d)
+        else:
+            pass
+    print("Finished Scraping!")
+    # browser.visit(button)
+    # new_dict = []
+    # for i in dict_list:
+
+    #     try:
+    #         browser.visit(i["Link"])
+    #         t.sleep(1)
+    #         html = browser.html
+    #         soup = BeautifulSoup(html, 'html.parser')
+    #         info_list = soup.find_all('p', class_ = 'cXenseParse')
+    #         i["Title"] = info_list[0].text
+    #         i["Blurb"] = info_list[1].text
+    #         new_dict.append(i)
+
+    #     except:
+    #         print("Error! trying to get")
---
+++ webscraping.py
@@ -0,0 +1,20 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+import re
+import time
+import requests
+from selenium import webdriver
+import time as t
+from pprint import pprint
+from scraper import scrape_site
+
+executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+browser = Browser('chrome', headless=False, **executable_path)
+try: 
+    url = 'https://www.bizjournals.com/houston/potm?l=&time=&type=&ind=86'
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    scrape_site(soup)
+except:
+    print("Error")
