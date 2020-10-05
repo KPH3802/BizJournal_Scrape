66da5c4fe757bddd7043f49b55a8ff6a5f0d6700
KPH3802 <kph3802@gmail.com>
2020-10-05 10:19:45 -0500

    Trying with Pandas dataframe

--- webscraping2.py
+++ webscraping2.py
@@ -60,18 +60,19 @@ def scrape_site(soup):
     except:
         print("Finished with next!")
         # print(Final_List)
-
+        df = pd.DataFrame(Final_List)
+        df.to_csv("Data_Scraped/Contacts.csv", index=False)
         # df = pd.DataFrame(dict_list)
         # df.to_csv("Contacts.csv", index=True)
-        csv_columns = ['Name','Link','Title', 'Blurb']
-        try:
-            with open(file_to_output, 'w') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-                writer.writeheader()
-                for data in Final_List:
-                    writer.writerow(data)
-        except IOError:
-            print("I/O error")
+        # csv_columns = ['Name','Link','Title', 'Blurb']
+        # try:
+        #     with open(file_to_output, 'w') as csvfile:
+        #         writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        #         writer.writeheader()
+        #         for data in Final_List:
+        #             writer.writerow(data)
+        # except IOError:
+        #     print("I/O error")
         # try:
         #   with open(file_to_output, 'w', encoding='utf8', newline='') as csvfile:
         #       writer = csv.DictWriter(csvfile, fieldnames=Final_List[0].keys())
